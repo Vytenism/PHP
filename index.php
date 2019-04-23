68f652310dcd1b6a4aad853b04b10a605604102d
vytenism <aparance23@gmail.com>
2019-04-23 23:55:56 +0300

    penkta uzduotis

--- index.php
+++ index.php
@@ -1,40 +1,48 @@
 <?php
 
-//$days = 365;
-
-$pakelio_kaina = 3.40;
+$pakelio_kaina = 3.75;
 $surukyta = 0;
 
-for ($day = 1; $day <= 365; $day++){
-   $weekday = date('N', strtotime("+$day days"));
+for ($day = 1; $day <= 365; $day++) {
+    $weekday = date('N', strtotime("+$day days"));
 
-   if ($weekday <= 4){
-       $cizos_mon_thu = rand(3, 4); // Nuo pirmadienio iki ketvirtadienio
-       $surukyta += $cizos_mon_thu; // Kiek surūkiau iki ketvirtadienio
-   } elseif ($weekday == 5){
-       $cizos_fri = rand(10, 20);
-       $surukyta += $cizos_fri;
-   } else {
-       $cizos_sat_sun = rand(1, 3);
-       $surukyta += $cizos_sat_sun;
-   }
-}
-$kainuos = round($surukyta / 20 * 3.75);
-$text = "per metus surukysiu $surukyta cigareciu uz $kainuos eur";
-
-    if ($weekday <= 4){ 
-    $kainos = ($cizos_mon_thu * $day) / 20 * 3.75;
+    if ($weekday <= 4) {
+        $cizos_mon_thu = rand(3, 4); // Nuo pirmadienio iki ketvirtadienio
+        $surukyta += $cizos_mon_thu; // Kiek surūkiau iki ketvirtadienio
+    } elseif ($weekday == 5) {
+        $cizos_fri = rand(10, 20); // Penktadienis
+        $surukyta += $cizos_fri; // Kiek surukiau per penktadieni
+    } else {
+        $cizos_sat_sun = rand(1, 3); // Sestadienis-sek
+        $surukyta += $cizos_sat_sun; // Kiek surukiau per sest-sek
     }
-    $sutaupysiu = "nerukydamas pirmad-ketvirtadieni sutaupyciau $kainos eur";
+}
+  
+$kainuos = round($surukyta / 20 * 3.75);
+$text = "Per metus surukysiu $surukyta cigareciu uz $kainuos eur.";
+$pakelio_kaina2 = 3.75;
+$surukyta2 = 0;
+
+for ($day = 1; $day <= 365; $day++) {
+    $weekday = date('N', strtotime("+$day days"));
+
+    if ($weekday <= 4) {
+        $cizos_mon_thu = rand(3, 4); // Nuo pirmadienio iki ketvirtadienio
+        $surukyta2 += $cizos_mon_thu; // Kiek surūkiau iki ketvirtadienio
+    }
+}
+$kainuos2 = round($surukyta2 / 20 * 3.75);
+$text2 = "Nerukydamas pirmadieni-ketvirtaideni sutaupyciau $kainuos2 eur.";
 
 ?>
+
 <html>
     <head>
         <title>main</title>
     </head>
     <body>
         <P>
-           <?php print $text . ' ' . $sutaupysiu ;?>
+            <?php print $text2 . ' ' . $text;?>
         </P>
     </body>
 </html>
