c5223b1391a1af9e6236e1696b9a61becae2f47f
vytenism <aparance23@gmail.com>
2019-04-23 15:43:35 +0300

    2 uzduotis

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php 
 
 $kates = rand(1, 3);
-$sunis = rand(1, 3);
+$sunys = rand(1, 3);
 
 $katasuniai = 0;
 
 for ($k = 1; $k <= $kates; $k++) {
-    for ($s = 1; $s <= $sunis; $s++) {
+    for ($s = 1; $s <= $sunys; $s++) {
         $success = rand(0,1);
         if ($success) {
             $katasuniai++;
@@ -15,7 +15,7 @@ for ($k = 1; $k <= $kates; $k++) {
     }
 }
 
-$text = "Event'e dalyvavo $kates kates ir $sunis sunys.<br>"
+$text = "Event'e dalyvavo $kates kates ir $sunys sunys.<br>"
         . "Katasuniu iseiga: $katasuniai";
 ?>
 <html>
