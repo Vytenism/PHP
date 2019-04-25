ce4ff324638d5edde39f9070759a0ff48d7bf650
vytenism <aparance23@gmail.com>
2019-04-25 14:57:08 +0300

    array

--- index.php
+++ index.php
@@ -1,25 +1,6 @@
 <?php
 
-$car_price_new = 30000; //Eurų
-$car_price = $car_price_new; // (% nuo esamos vertės per mėnesį)
-$santaupos = 15000;
-
-for ($m = 0; $car_price >= $santaupos; $m++){
-    $car_price -= round($car_price*0.02);    
-    print "Car price at $m = $car_price<br>";
-} 
-
-$likutis = $santaupos - $car_price;
-$text = "Po $m menesiu, masinos verte bus $car_price. nusipirkus tau dar liks $likutis eur."
-
-?>
-<html>
-    <head>
-        <title>asdv</title>
-    </head>
-    <body>
-        <p>
-           <?php print $text; ?> 
-        </p>
-    </body>
-</html>
+$array2 = [1, 2, 3, 4];
+$array2[5] = 5;
+var_dump($array2); 
+// viska meta
