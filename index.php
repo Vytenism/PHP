00d16db6d5d46ec68d78b897470567c6d4d4f60d
vytenism <aparance23@gmail.com>
2019-04-25 15:05:34 +0300

    array

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
-$array2 = [1, 2, 3, 4];
-$array2[5] = 5;
-var_dump($array2); 
-// viska meta
+$array = [
+    'vaisius' => 'Oboulys',
+    'vaisius2' => 'Apelsinas',
+    'darzove' => 'Cukinija',
+];
+var_dump ($array);
