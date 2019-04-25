7bf19138fdd39e0ac7dfd0871a7912fd72baca66
vytenism <aparance23@gmail.com>
2019-04-25 16:07:12 +0300

    array

--- index.php
+++ index.php
@@ -1,13 +1,42 @@
 <?php
 
-$produktai = [
-    'size' => [
-        'du dideli kiausiniai',
-        'vienas didelis grietines indelis'
+$arr = [
+    'kiausiniai' => [
+        'name' => 'Kiausinis',
+        'amount' => 2,
+        'size' => 'didelis'
     ],
-    'amount' => [
-        'vienas mazas baklazanas',
-        'vienas didelis grietines indelis'
+    'baklazanas' => [
+        'name' => 'Baklazanas',
+        'amount' => 1,
+        'size' => 'mazas'
         ],
+    'grietine' => [
+        'name' => 'Grietine',
+        'amount' => 1,
+        'size' => 'didelis'
+    ]
 ];
-var_dump ($produktai);
+
+print $arr['grietine']['amount'];
+var_dump ($arr);
+
+?>
+<html>
+    <head>
+        <title>array</title>
+    </head>
+    <body>
+        <div class="produktas">
+            <span class="pavadinimas">
+                <?php print $arr['grietine']['name'];?>
+            </span>
+            <span class="kiekis">
+                 <?php print $arr['grietine']['amount'];?>
+            </span>
+            <span class="dydis">
+                 <?php print $arr['grietine']['size'];?>
+            </span>
+        </div>   
+    </body>
+</html>
