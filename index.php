26fad129c031bfc000714154f2786059cc7759cb
vytenism <aparance23@gmail.com>
2019-04-30 16:36:22 +0300

    array 5uzduotis 2 budas

--- index.php
+++ index.php
@@ -1,31 +1,70 @@
 <?php
-$daiktai = ['Kremas','Riestuai','Lupdazis','Kojnes','Raktai'];
-$size = rand(1, 20);
-$is_dark = rand(0, 1);
 
-$rand_name = rand(0, count($daiktai)-1);
+$gerimai = [
+    [
+        'name' => 'Vilkmerges alus',
+        'kaina' => 4.50,
+        'nuolaida' => 0
+    ],
+    [
+        'name' => 'Stumbro degtine',
+        'kaina' => 8.50,
+        'nuolaida' => 10
+    ],
+    [
+        'name' => 'Svyturio alus',
+        'kaina' => 3.50,
+        'nuolaida' => 15
+    ],
+    [
+        'name' => 'Zemaitijos pienas',
+        'kaina' => 1.50,
+        'nuolaida' => 30
+    ],
+];
 
-if($is_dark){
-    $color = 'tamsus';
-} else {
-    $color = 'sviesus';
+foreach ($gerimai as $index => &$gerimas) {
+    if ($gerimas['nuolaida'] > 0) {
+        $gerimas['css_class'] = 'big_font';
+//        $kaina_su_nuolaida = round($gerimas['kaina'] * (100 - $gerimas['nuolaida']) / 100);
+//        $gerimai[$index]['kaina_su_nuolaida'] = $kaina_su_nuolaida;
+    } else {
+        $gerimas['css_class'] = 'small_font';
+    }
 }
 
-$text = "$daiktai[$rand_name] uzima $size cm3. Daiktas $color.";
+var_dump($gerimai);
 ?>
+<!DOCTYPE html>
 <html>
     <head>
-        <title></title>
+        <title>Gerimai</title>
         <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
         <style>
+            .big_font {
+                font-size: 50px;
+            }
+            
+            .big_font .kaina {
+                text-decoration: line-through;
+            }
         </style>
     </head>
     <body>
-        <p><?php print $text ;?></p>
+        <ul>
+            <?php foreach ($gerimai as $gerimas): ?>
+                <li class="<?php print $gerimas['css_class']; ?>">
+                    <span><?php print $gerimas['name']; ?></span>
+                    <?php if (isset($gerimas['kaina_su_nuolaida'])): ?>
+                        <span><?php print $gerimas['kaina_su_nuolaida']; ?></span>
+                    <?php endif; ?>
+                    <span class="kaina"><?php print $gerimas['kaina']; ?></span>
+                </li>
+            <?php endforeach; ?>
+        </ul>
     </body>
 </html>
-
 
 
 
