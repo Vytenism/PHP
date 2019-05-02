38cb5a0c4fda107f8bb5b12041f4cd3dee4da051
vytenism <aparance23@gmail.com>
2019-05-02 14:36:37 +0300

    function

--- index.php
+++ index.php
@@ -1,43 +1,16 @@
 <?php
 
-function slot_run() {
-    $array = [
-        [rand(0, 1), rand(0, 1), rand(0, 1)],
-        [rand(0, 1), rand(0, 1), rand(1, 2)],
-        [rand(0, 1), rand(1, 2), rand(0, 1)]
-    ];
-    return $array;
+function slot_run(){
+    $square = [];
+    for($x = 1; $x <= 5; $x++){
+        $row = [];
+        for($z = 1; $z <=5; $z++){
+            $numbers = rand(0, 1);
+            $row []= $numbers;
+        }
+        $square[] = $row;
+    }
+    return $square;
 }
 
-$array1 = slot_run();
-//foreach ($array1 as $index) {
-//    foreach ($index as $color) {
-//        if ($color > 0) {
-//            $array1[$color]['css_class'] = 'blue';
-//        } else {
-//            $array1[$color]['css_class'] = 'orange';
-//        }
-//    }
-//}
-?>
-
-<html>
-    <head>
-        <title>function's</title>
-        <link rel="stylesheet" type="text/css" href="style.css">
-        <meta charset="UTF-8">
-    </head>
-    <body>
-            <?php foreach ($array1 as $index): ?>
-            <section>
-            <?php foreach ($index as $value): ?>
-                    <?php if ($value > 0): ?>
-                        <div class="blue"></div>
-                    <?php else: ?>
-                        <div class="orange"></div>
-                    <?php endif; ?>
-                <?php endforeach; ?>
-            </section>
-            <?php endforeach; ?>
-    </body>
-</html>
+var_dump(slot_run());
