5baebe604ac8842a9006765b781481ec0b6840f6
vytenism <aparance23@gmail.com>
2019-05-02 16:51:28 +0300

    two functions to generate squares

--- index.php
+++ index.php
@@ -11,34 +11,56 @@ function slot_run($size){
         $row = [];
         
         for($z = 1; $z <=$size; $z++){ 
-            $row[] = rand(0, 1);
+            $row[] = rand(0, 2);
         }
         $square[]=$row;   
     }
     return $square;
 }
+function winning_slot($matrix){
+    $winning_rows = [];
+    
+    foreach ($matrix as $index => $row){
+        $row_winning_sum= count($row);
+        $column_sum = 0;
+        
+        foreach ($row as $column){
+            $column_sum += $column ;
+        }
+        if ($row_winning_sum == $column_sum){
+            $winning_rows[] = $index;
+        }
+        if ($column_sum == 0){
+            $winning_rows[] = $index;
+        }
+    }
+    
+    return $winning_rows;
+}
 
-$size = rand(1, 5);
+$size = rand(1, 15);
 $array = slot_run($size);
-
+$winning_rows = winning_slot($array);
+var_dump ($winning_rows);
 ?>
-
 <html>
     <head>
         <title>functions</title>
-        <<link rel="stylesheet" href="style.css"/>
+        <link rel="stylesheet" href="style.css"/>
+        <meta charset="UTF-8">
+        <meta http-equiv="refresh" content="1"/> 
     </head>
     <body>
         <?php foreach ($array as $index => $value): ?>
-    <section>
+            <section>
         <?php foreach ($value as $index => $random): ?>
-            <?php if ($random): ?>
+            <?php if ($random<1): ?>
                 <div class="blue"></div>
             <?php else: ?>
                 <div class="orange"></div>
             <?php endif; ?>
         <?php endforeach; ?>
-    </section>
+            </section>
         <?php endforeach; ?>
     </body>
 </html>
